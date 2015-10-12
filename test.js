19e78a6894a45486d118fcddb00dfa2dc345e8ae
ashwini.adke <aadke@ebay.com>
2015-10-12 15:49:14 -0700

    added chai

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-ert = require("assert");
+var assert = require("assert");
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
@@ -6,4 +6,17 @@ describe('Array', function() {
       assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
+});
+
+var chai = require("chai");
+var foo = "bar";
+var tea ={'flavors':[23,32,55]};
+var assert = chai.assert;
+describe('hello',function(){
+
+assert.typeOf(foo, 'string');
+assert.equal(foo, 'bar');
+assert.lengthOf(foo, 3)
+assert.property(tea, 'flavors');
+assert.lengthOf(tea.flavors, 3);
 });
